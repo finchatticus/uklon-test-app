68618653b660ce5f6cc5daf39880bbfd5cd61c48
vlad <kozii.vladyslav@gmail.com>
2020-05-24 13:01:30 +0300

    Init commit

---
+++ app/src/main/kotlin/ua/vlad/uklon/presentation/ContainerActivity.kt
@@ -0,0 +1,6 @@
+package ua.vlad.uklon.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import ua.vlad.uklon.R
+
+class ContainerActivity : AppCompatActivity(R.layout.activity_container)
