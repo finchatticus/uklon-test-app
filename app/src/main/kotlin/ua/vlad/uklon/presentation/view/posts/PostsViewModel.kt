b6b4ebbc8a401ae3bf0e77870205ada058d12a0b
vlad <kozii.vladyslav@gmail.com>
2020-05-25 11:24:10 +0300

    Add register disposables for RxJava

--- app/src/main/kotlin/ua/vlad/uklon/presentation/view/posts/PostsViewModel.kt
+++ app/src/main/kotlin/ua/vlad/uklon/presentation/view/posts/PostsViewModel.kt
@@ -1,20 +1,21 @@
 package ua.vlad.uklon.presentation.view.posts
 
 import android.util.Log
-import androidx.lifecycle.ViewModel
 import io.reactivex.rxjava3.kotlin.subscribeBy
 import ua.vlad.uklon.domain.model.Post
 import ua.vlad.uklon.domain.usecase.GetPostsUseCase
 import ua.vlad.uklon.domain.usecase.RefreshPostUseCase
+import ua.vlad.uklon.presentation.common.BaseViewModel
 import ua.vlad.uklon.presentation.common.Status
 import ua.vlad.uklon.presentation.common.StatusLiveData
 import ua.vlad.uklon.presentation.common.applySchedulers
 
-class PostsViewModel(private val getPostsUseCase: GetPostsUseCase, private val refreshPostUseCase: RefreshPostUseCase) : ViewModel() {
+class PostsViewModel(private val getPostsUseCase: GetPostsUseCase, private val refreshPostUseCase: RefreshPostUseCase) : BaseViewModel() {
 
     val postsLiveData = StatusLiveData<List<Post>>()
 
     fun fetchPosts() {
+        disposeAll()
         postsLiveData.value = Status.Loading
         getPostsUseCase.getAll()
             .applySchedulers()
@@ -22,9 +23,11 @@ class PostsViewModel(private val getPostsUseCase: GetPostsUseCase, private val r
                 onNext = { postsLiveData.value = Status.Success(it) },
                 onError = { postsLiveData.value = Status.Error(it) }
             )
+            .registerDisposable()
     }
 
     fun refreshPosts() {
+        disposeAll()
         postsLiveData.value = Status.Loading
         refreshPostUseCase.refresh()
             .applySchedulers()
@@ -32,6 +35,7 @@ class PostsViewModel(private val getPostsUseCase: GetPostsUseCase, private val r
                 onNext = { postsLiveData.value = Status.Success(it) },
                 onError = { postsLiveData.value = Status.Error(it) }
             )
+            .registerDisposable()
     }
 
     fun onPostClicked(post: Post) {
